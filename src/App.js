488206bc3c115c033e2b773a69ebc4a501fe869b
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-21 10:17:35 +0200

    Checking state of text variable on h1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Form from'./components/Form';
+import TodoList from "./components/TodoList";
+import {useState} from "react";
+
 
 function App() {
+    const [text, setText] = useState("");
+
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+        <header>
+            <h1>Todo List{text}</h1>
+        </header>
+         <Form setText={setText} />
+        <TodoList/>
     </div>
   );
 }
