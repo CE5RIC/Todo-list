91f24b8549b51f8cb2df84e5905c3cf3b8a71916
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-23 10:01:40 +0200

    function created to delete todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,20 @@
-const Todo = (text) => {
+const Todo = ({text, todos, todo, setTodos}) => {
+
+    // function to find an element that matches with whatever the user clicked on, if it matches it gets deleted
+    const deleteHandler = () => {
+     setTodos(todos.filter((el) => el.id !== todo.id));
+    };
+
+
+
     return(
 <div className="todo">
     <li className="todo-item">{text}</li>
     <button className="complete-btn">
-
+        
     </button>
-    <button className="trash-btn">
-
+    <button onClick={deleteHandler} className="trash-btn">
+        
     </button>
 </div>
     );
