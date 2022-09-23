8488afee0b5214b26c7f213e4a6bd005974fb8ea
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-23 16:56:16 +0200

    typo fixed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,21 +2,29 @@ const Todo = ({text, todos, todo, setTodos}) => {
 
     // function to find an element that matches with whatever the user clicked on, if it matches it gets deleted
     const deleteHandler = () => {
-     setTodos(todos.filter((el) => el.id !== todo.id));
+        setTodos(todos.filter((el) => el.id !== todo.id));
     };
 
-
+    const completeHandler = () => {
+        setTodos(todos.map((el) => {
+                if(el.id === todo.id) {
+                    return {
+                        ...el, completed: !el.completed
+                    };
+                }
+                return el;
+            })
+        )
+    }
 
     return(
-<div className="todo">
-    <li className="todo-item">{text}</li>
-    <button className="complete-btn">
-        
-    </button>
-    <button onClick={deleteHandler} className="trash-btn">
-        
-    </button>
-</div>
+        <div className="todo">
+            <li className={`todo-item ${todo.completed ? "completed" : ''}`}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn">
+            </button>
+            <button onClick={deleteHandler} className="trash-btn">
+            </button>
+        </div>
     );
 }
 
